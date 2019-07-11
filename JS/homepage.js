b956e2eb2dd6a09c2500e4a71591ed15e3edd464
ionascuandrei <andrei.ionascu.97@gmail.com>
2019-07-11 12:19:15 +0300

    Visual work

--- JS/homepage.js
+++ JS/homepage.js
@@ -55,4 +55,38 @@ window.onload = function() {
 
         content.appendChild(div_hover);
     }
+
+
+    //==================== Log IN Section =================================
+
+    // Get the modal
+    var modalLogin = document.getElementById('id01');
+    var modalSignUp = document.getElementById('id02');
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modalLogin) {
+            modalLogin.style.display = "none";
+        }
+        if (event.target == modalSignUp) {
+            modalSignUp.style.display = "none";
+        }
+    }
+
+    document.getElementById("loginButton").onclick = function(event) {
+        var userName = document.getElementsByName("uname")[0];
+        var password = document.getElementsByName("psw")[0];
+        console.log("Aici sunt");
+
+        if(userName.value === "nic_irina" && password.value === "micro")
+        {
+            userName.parentNode.parentNode.action = "http://localhost:3000/selectMode.html";
+        }
+        else {
+            alert("Incorrect username or password!");
+        }
+        console.log("sadfsdf");
+    }
+
+
 }
