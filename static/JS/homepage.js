fdfb52f2e3ca8623ba91b5dc6bcb910f9dce2bb0
ionascuandrei <andrei.ionascu.97@gmail.com>
2019-07-12 10:53:25 +0300

    Getting Occupied washing machines from DB

--- static/JS/homepage.js
+++ static/JS/homepage.js
@@ -1,81 +1,94 @@
 nr_washing_machines = 8; // va fi extras din baza de date a spalatoriei curente
 
 window.onload = function() {
+          fetch('../washingpoints')
+              .then(function(response) {
+                  return response.json();
+              })
+              .then(function(myJson) {
+                var wm = myJson.result[0].washing_machines;
+                  console.log(wm);
 
-    // document.body.appendChild();
-    // for(let i=0; i<nr_washing_machines; i++) {
-    //     let daily_program = document.getElementById("today_program").cloneNode(true);
-    //     daily_program.setAttribute("class", "hoverShow");
-    //     daily_program.setAttribute("style", "display: block");
-    // }
 
-    // var content = document.createElement("DIV");
-    // content.setAttribute("id", "container");
-    // document.body.appendChild(content);
-    for(let i=0; i<nr_washing_machines; i++) {
-        var div_hover = document.createElement("DIV");
-        div_hover.setAttribute("class", "hovereffect");
+            // document.body.appendChild();
+            // for(let i=0; i<nr_washing_machines; i++) {
+            //     let daily_program = document.getElementById("today_program").cloneNode(true);
+            //     daily_program.setAttribute("class", "hoverShow");
+            //     daily_program.setAttribute("style", "display: block");
+            // }
 
-        var div_text_program = document.createElement("DIV");
-        div_text_program.setAttribute("class", "overlay");
-        var link_text = document.createElement("A");
-        link_text.setAttribute("class", "info");
-        // link_text.setAttribute("href", "washing_calendar.html")
+            // var content = document.createElement("DIV");
+            // content.setAttribute("id", "container");
+            // document.body.appendChild(content);
+            for(let i=0; i<nr_washing_machines; i++) {
+                var div_hover = document.createElement("DIV");
+                div_hover.setAttribute("class", "hovereffect");
 
-        let text_received;
-        // fetch('http://localhost:3000/')
-        //     .then((text_received) => {
-        //         link_text.innerText = text_received;
-        //         console.log(link_text);
-        //     });
+                var div_text_program = document.createElement("DIV");
+                div_text_program.setAttribute("class", "overlay");
+                var link_text = document.createElement("A");
+                link_text.setAttribute("class", "info");
+                // link_text.setAttribute("href", "washing_calendar.html")
 
-        fetch('../users')
-            .then(function(response) {
-                return response.json();
-            })
-            .then(function(myJson) {
-                link_text.innerText = text_received;
-                // console.log(myJson);
-            });
+                let text_received;
+                // fetch('http://localhost:3000/')
+                //     .then((text_received) => {
+                //         link_text.innerText = text_received;
+                //         console.log(link_text);
+                //     });
 
-        var daily_program = document.getElementById("today_program").cloneNode(true);
-        daily_program.setAttribute("style", "display: block");
 
-        var input_hour = daily_program.children;
-        console.log(input_hour);
-        let verif_culori = 0;
-        for(let i=0; i<input_hour.length; i++) {
-            var line = input_hour[i];
-            if(verif_culori%2) {        // conditia din DB pt program ocupat
-                line.setAttribute("class", "available");
-            }
-            else {
-                line.setAttribute("class", "occupied");
+                        var daily_program = document.getElementById("today_program").cloneNode(true);
+                        daily_program.setAttribute("style", "display: block");
 
-            }
-            verif_culori++;
-            console.log(line);
-        }
+                        var input_hour = daily_program.children;
+                        console.log(input_hour);
+                        let verif_culori = 0;
 
-        link_text.appendChild(daily_program);
-        div_text_program.appendChild(link_text);
+                        // for(int j = 0; j < i; j++){
+                        //   if(wm[i].occupied.length != 0) {
+                        //     for(int k = 0; k < wm[i].occupied.length; k++) {
+                        //       wm[i].occupied[k].start_time.
+                        //       input_hour[] .setAttribute("class", "occupied");
+                        //     }
+                        //   }
+                        // }
 
-        var link_machine = document.createElement("A");
-        link_machine.setAttribute("href", "washing_calendar.html");
-        console.log(link_machine.href);
+                        for(let i=0; i<input_hour.length; i++) {
+                            var line = input_hour[i];
+                            if(verif_culori%2) {        // conditia din DB pt program ocupat
+                                line.setAttribute("class", "available");
+                            }
+                            else {
+                                line.setAttribute("class", "occupied");
+                            }
+                            verif_culori++;
+                            console.log(line);
+                        }
 
-        var w_machine = document.createElement("IMG");
-        w_machine.setAttribute("src", "../images/washing-machine.png");
-        w_machine.setAttribute("alt", "washing_machine_nr_" + i);
-        w_machine.setAttribute("width", "80%");
-        w_machine.setAttribute("height", "80%");
+                        link_text.appendChild(daily_program);
+                        div_text_program.appendChild(link_text);
 
-        link_machine.appendChild(w_machine);
-        div_hover.appendChild(link_machine);
-        div_hover.appendChild(div_text_program);
+                        var link_machine = document.createElement("A");
+                        link_machine.setAttribute("href", "washing_calendar.html");
+                        console.log(link_machine.href);
 
-        document.body.appendChild(div_hover);
-    }
+                        var w_machine = document.createElement("IMG");
+                        w_machine.setAttribute("src", "../images/washing-machine.png");
+                        w_machine.setAttribute("alt", "washing_machine_nr_" + i);
+                        w_machine.setAttribute("width", "80%");
+                        w_machine.setAttribute("height", "80%");
+
+                        link_machine.appendChild(w_machine);
+                        div_hover.appendChild(link_machine);
+                        div_hover.appendChild(div_text_program);
+
+                        document.body.appendChild(div_hover);
+                    }
+
+
+              });
+
 
 
     //==================== Log IN Section =================================
